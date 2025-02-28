c7bc37a176f3b189c848f28621bde6181ec0d3ab
akeller3333 <akeller3@norwich.edu>
2025-02-28 04:37:55 -0500

    Create flippingBits.c

---
+++ flippingBits.c
@@ -0,0 +1,8 @@
+// You will be given several 32 bit unsigned integers. Flip all the bits (1->0 and 0->1) and return the result as an unsigned integer.
+// Input: A long integer
+
+long flippingBits(long n) {
+
+    return UINT32_MAX^n;
+
+}
